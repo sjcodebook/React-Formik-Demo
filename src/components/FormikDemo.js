6e152a02bb0963d0b7b312f227be395e05da7be2
Sanmati Kumar Jain <i.sanmati.jain@redcarpetup.com>
2020-07-29 03:30:57 +0530

    build form structure

--- src/components/FormikDemo.js
+++ src/components/FormikDemo.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
-import { Form, Formik } from 'formik';
+import { Form, Formik, Field } from 'formik';
+
+const initialValues = {
+  fullName: '',
+  initialInvestment: 0,
+  investmentRisk: [],
+  commentAboutInvestmentRisk: '',
+  dependents: -1,
+  acceptedTermsAndConditions: false,
+};
 
 export const FormikDemo = () => {
   return (
     <Card>
       <CardContent>
         <Typography variant="h4">New Account</Typography>
+        <Formik initialValues={initialValues} onSubmit={() => {}}>
+          {({ values }) => (
+            <Form>
+              <Field name="fullName" />
+              <Field name="initialInvestment" type="number" />
+              <Field name="investmentRisk" value="High" type="checkbox" />
+              <Field name="investmentRisk" value="Medium" type="checkbox" />
+              <Field name="investmentRisk" value="Low" type="checkbox" />
+              <Field name="commentAboutInvestmentRisk" as="textarea" />
+              <Field name="dependents" as="select">
+                <option value={0}>0</option>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+              </Field>
+              <Field name="acceptedTermsAndConditions" type="checkbox" />
+              <pre>{JSON.stringify(values, null, 4)}</pre>
+            </Form>
+          )}
+        </Formik>
       </CardContent>
     </Card>
   );
