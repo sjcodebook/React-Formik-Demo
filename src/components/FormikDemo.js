59af058db86adbfbe362adaf777f9ee8a86baea4
Sanmati Kumar Jain <i.sanmati.jain@redcarpetup.com>
2020-07-29 03:12:52 +0530

    init

---
+++ src/components/FormikDemo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
+import { Form, Formik } from 'formik';
+
+export const FormikDemo = () => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h4">New Account</Typography>
+      </CardContent>
+    </Card>
+  );
+};
